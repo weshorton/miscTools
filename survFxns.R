aeb1770cc86ad6cb33c5191255894960a6656e5e
Wesley Horton <hortowe@ohsu.edu>
2018-09-12 10:02:14 -0700

    Added new function for additive survival fitting

--- survFxns.R
+++ survFxns.R
@@ -5,6 +5,7 @@
 ######## Table of Contents ########
 ### comboSurvFit                ###
 ### addSubCols                  ###
+### additiveSurvFit             ###
 ###################################
 
 ### Functions that help during survival analysis
@@ -155,3 +156,93 @@ addSubCols <- function(data_df){
 #     } # fi
 #   } # for col_v
 # } # fi
+
+
+### ADDITIVE SURV FIT
+additiveSurvFit <- function(genes_v, data_df, cols_v, median_log, groups_v = c("lo_lo", "up_up")){
+  ## genes_v - vector of 1 or 2 genes to combine counts
+  ## data_df - count data where rows = patient/sample and columns = genes
+  ## cols_v - clinical information columns contained in data_df that are used for survival analysis
+    ## First one should be response and second should be time.
+  ## median_log - TRUE - replace NA value sof each gene w/ its median value. FALSE - keep NAs
+  ## groups_v - Character vector containing any and all of: 
+    ## "lo_lo", "lo_up", "up_lo", and "up_up"
+  
+  ## Subset data
+  #clin_df <- data_df[,cols_v]
+  sub_df <- data_df[,c(cols_v, genes_v), drop = F]
+  
+  ## Add median values, if specified
+  if (median_log) {
+    sub_df <- as.data.frame(apply(sub_df, 2, function(x){
+      naRow_v <- which(is.na(x))
+      med_v <- median(x, na.rm = T)
+      x[naRow_v] <- med_v
+      return(x)
+    }))
+  } # fi
+  
+  ## Find out how many rows have NA in one of the two count columns
+  naRow_v <- which(apply(sub_df[,genes_v], 1, function(x) {
+    length( which( is.na(x) ) ) > 0
+  }))
+  
+  ## Skip if fewer than 20% of patients remain
+  if (length(naRow_v) > nrow(sub_df)*.8){
+    print(sprintf("Skipping combo %s. Fewer than 20 pct of patients had full measurements",
+                  paste(genes_v, collapse = "_")))
+    return(list("values" = NA, "sig" = NA, "diff" = NA, "fit" = NA))
+  }
+  
+  ## Get medians for each analyte
+  medians_v <- sapply(genes_v, function(x) median(sub_df[[x]], na.rm = T))
+  names(medians_v) <- genes_v
+  
+  ## Get above and below for each analyte
+  updown_lslsv <- sapply(genes_v, function(x){
+    currLo_v <- which(sub_df[[x]] < medians_v[x])
+    currUp_v <- which(sub_df[[x]] >= medians_v[x])
+    return(list("lo" = currLo_v, "up" = currUp_v))
+  }, simplify = F)
+  names(updown_lslsv) <- genes_v
+  
+  ## Add expression group info to data
+  groupCols_v <- character(length = length(genes_v))
+  for (i in 1:length(genes_v)){
+    ## Get column name
+    currCol_v <- paste0(genes_v[i], ".Exp.Grp")
+    groupCols_v[i] <- currCol_v
+    ## Create new column to hold exp groups and make them all low
+    sub_df[[currCol_v]] <- "lo"
+    ## Change those that should actually be high
+    sub_df[updown_lslsv[[ genes_v[i] ]]$up, eval(currCol_v)] <- "up"
+  }
+    
+  ## Create empty column to hold combo
+  sub_df$combo <- character(length = nrow(sub_df))
+  
+  ## Add new columns
+  sub_df$combo <- apply(sub_df[,groupCols_v], 1, paste, collapse = "_")
+  
+  ## Subset
+  sub_df <- sub_df[sub_df$combo %in% groups_v,]
+  
+  ## Check groups
+  allGroups_v <- unique(sub_df$combo)
+  
+  if (length(allGroups_v) < 2){
+    survDiff <- pval <- survFit <- NA
+  } else {
+    ## Create survival object
+    surv_obj <- Surv(time = sub_df[[ cols_v[2] ]], sub_df[[ cols_v[1] ]])
+    
+    ## Calculate survdiff, pvalue, and fit
+    survDiff <- survdiff(surv_obj ~ sub_df$combo)
+    pval <- pchisq(survDiff$chisq, df = 1, lower = F)
+    survFit <- survfit(surv_obj ~ sub_df$combo)
+  }
+  
+  ## Return
+  return(list("values" = sub_df, "sig" = pval, "diff" = survDiff, "fit" = survFit))
+  
+}
