24d0cbcf2eabd504a0e8c3ca35ae7a0b08e52372
Wesley Horton <hortowe@ohsu.edu>
2018-10-02 08:54:59 -0700

    Updated legend plotting in kmPlot(). Removed sourcing of quantile heat and textToexcel

--- utilityFxns.R
+++ utilityFxns.R
@@ -33,7 +33,6 @@ library(grid); library(gridExtra); library(gtable)
 ###   nChooseK                  Find # of unique pairwise combos         ###
 ###   dupCols                   Find duplicated columns (and remove)     ###
 ###   thirds                    Get values of min/low3rd/med/upp3rd/max  ###
-###   textToExcel               Convert dir of csv/tsv files to xlsx     ###
 ###   convertDFT                Convert df to dt and vice versa          ###
 ###   kmPlot                    Custom Kaplain Meier plot                ###
 ###   quantileHeat              Heatmap with quantile color scale        ###
@@ -946,7 +945,8 @@ convertDFT <- function(data_dft, col_v = NA, newName_v = "V1") {
 ###
 
 kmPlot <- function(fit, model, colors_v = c("blue", "red"), labels_v = c("Low" = "low", "High" = "up"), main_v = "KM Survival", 
-                   xlab_v = "Overall Survival", ylab_v = "Prop. Survival", max_x, leg_y = c('med' = 1, 'rec' = 0.73, 'p' = 0.5)) {
+                   xlab_v = "Overall Survival", ylab_v = "Prop. Survival", max_x, leg_y = c('med' = 1, 'rec' = 0.83, 'p' = 0.7),
+                   fileName_v = NA) {
   #' Custom Kaplan Meier plot with median lines and legends
   #' @description Specific Kaplan Meier plot
   #' @param fit "survfit" object created by survfit() function
@@ -958,9 +958,11 @@ kmPlot <- function(fit, model, colors_v = c("blue", "red"), labels_v = c("Low" =
   #' @param ylab_v label for y-axis. Default is "Prop. Survival"
   #' @param max_x Max x-value from original data that created fit. Used for placing legends
   #' @param leg_y named vector of legend y-axis placements. Names are 'med', 'p', and 'rec'. Legends will only be printed if they have a leg_y value.
+  #' @param fileName_v path to output file where plot will be printed. If NA, will print to stdout.
   #' @value plot to console of Kaplan Meier survival estimate
   #' @export
   
+  if (!is.na(fileName_v)) pdf(file = fileName_v, width = 10, height = 10)
   ## Generate base plot
   plot(fit, col = colors_v, frame = F, lwd = 2,
        main = main_v, xlab = xlab_v, ylab = ylab_v,
@@ -974,24 +976,44 @@ kmPlot <- function(fit, model, colors_v = c("blue", "red"), labels_v = c("Low" =
   lines(c(0, max(medians_v)), c(0.5, 0.5), lty = "dashed")
   mapply(function(x,y) lines(rep(x,2),c(0,0.5),col=y, lty="dashed"), medians_v, colors_v)
   
-  ## Add median legend
-  if ('med' %in% names(leg_y)) {
-    med_leg <- sapply(names(labels_v), function(x) paste(x, medians_v[[labels_v[x] ]], sep = " = "))
-    legend(max_x, leg_y['med'], legend = med_leg, col = colors_v, title = "Med. Surv.", bty = 'n', xjust = 1, cex = 1.2)
-  } # fi
+  ## Construct median and record legend
+  if ('med' %in% names(leg_y) & 'rec' %in% names(leg_y)) {
+    final_leg <- sapply(names(labels_v), function(x) {
+      paste0(x, " = ", medians_v[[ labels_v[x] ]], " ; ", records_v[[ labels_v[x] ]])})
+    legendTitle_v <- "Med. Surv. ; N. Records"
+  } else if ('med' %in% names(leg_y) & !('rec' %in% names(leg_y))) {
+    final_leg <- sapply(names(labels_v), function(x) paste(x, medians_v[[labels_v[x] ]], sep = " = "))
+    legendTitle_v <- "Med. Surv."
+  } else if (!('med' %in% names(leg_y)) & 'rec' %in% names(leg_y)) {
+    final_leg <- sapply(names(labels_v), function(x) paste(x, records_v[[ labels_v[x] ]], sep = " = "))
+    legendTitle_v <- "N. Records"
+  }
   
-  ## Add Record legend
-  if ('rec' %in% names(leg_y)) {
-    rec_leg <- sapply(names(labels_v), function(x) paste(x, records_v[[ labels_v[x] ]], sep = " = "))
-    legend(max_x, leg_y['rec'], legend = rec_leg, col = colors_v, title = "N. Records", bty = 'n', xjust = 1, cex = 1.2)
-  } # fi
+  ## Make it
+  if ('med' %in% names(leg_y) | 'rec' %in% names(leg_y)){
+    legend("topright", legend = final_leg, col = colors_v, title = legendTitle_v, bty = "n", xjust = 1, cex = 1.2, lwd = 2)
+  }
+  # ## Add median legend
+  # if ('med' %in% names(leg_y)) {
+  #   med_leg <- sapply(names(labels_v), function(x) paste(x, medians_v[[labels_v[x] ]], sep = " = "))
+  #   legend(max_x, leg_y['med'], legend = med_leg, col = colors_v, title = "Med. Surv.", bty = 'n', xjust = 1, cex = 1.2, lwd = 2)
+  # } # fi
+  # 
+  # ## Add Record legend
+  # if ('rec' %in% names(leg_y)) {
+  #   rec_leg <- sapply(names(labels_v), function(x) paste(x, records_v[[ labels_v[x] ]], sep = " = "))
+  #   legend(max_x, leg_y['rec'], legend = rec_leg, col = colors_v, title = "N. Records", bty = 'n', xjust = 1, cex = 1.2, lwd = 2)
+  # } # fi
   
   ## Add pvalue legend
   if ('p' %in% names(leg_y)) {
     pval <- pchisq(model$chisq, df = 1, lower = F)
     pv_leg <- paste0("p.val = ", round(pval, digits = 4))
-    legend(max_x, leg_y['p'], legend = pv_leg, bty = 'n', xjust = 1, cex = 1.2)
+    legend("bottomleft", legend = pv_leg, bty = 'n', xjust = 1, cex = 1.2)
   }
+  
+  ## Close device
+  if (!is.na(fileName_v)) graphics.off()
   
 }
 
@@ -999,11 +1021,5 @@ kmPlot <- function(fit, model, colors_v = c("blue", "red"), labels_v = c("Low" =
 ### Quantile Heat ###############################################################################################################
 ###
 
-dir_v <- dirname(sys.frame(1)$ofile)
-source(file.path(dir_v, "quantileHeatmap.R"))
-
-###
-### text to excel ###############################################################################################################
-###
-
-source(file.path(dir_v, "textToExcel.R"))
+#dir_v <- dirname(sys.frame(1)$ofile)
+source("~/my_tool_repos/WesPersonal/quantileHeatmap.R")
